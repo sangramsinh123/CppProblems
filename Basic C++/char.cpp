03d198f32e1a5cdc02a60efefa98ecef066e5f29
Sangramsinh Sawant <49286586+sangramsinh123@users.noreply.github.com>
2022-07-07 19:47:06 +0530

    Create char.cpp

---
+++ Basic C++/char.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    // std::cout << "Hello world!";
+    cout << (char)97 << endl;
+    cout << (char)98 << endl;
+    return 0;
+}
