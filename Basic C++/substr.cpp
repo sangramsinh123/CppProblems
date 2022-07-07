136b1eee0bb12211089716067ee781e767e97f24
Sangramsinh Sawant <49286586+sangramsinh123@users.noreply.github.com>
2022-07-07 12:07:05 +0530

    Add files via upload

---
+++ Basic C++/substr.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    
+    string s = "sangram";
+    //          0123456
+    
+    cout << s.substr(0,3) << endl;//O/P = san
+    cout << s.substr(0) << endl;//O/P = sangram
+    return 0;
+}
