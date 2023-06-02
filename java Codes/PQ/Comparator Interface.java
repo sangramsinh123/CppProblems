4c0d1270708ab3144bdda16bb6376277736ad711
Sangramsinh Sawant <49286586+sangramsinh123@users.noreply.github.com>
2023-06-02 11:57:35 +0530

    Create Comparator Interface.java

---
+++ java Codes/PQ/Comparator Interface.java
@@ -0,0 +1,76 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.*;
+
+class student{
+    int maths;
+    int physics;
+    // implement getters and setters according to our need
+    public student(int maths , int physics){
+        this.maths = maths;
+        this.physics = physics;
+    }
+}
+
+
+class cmp implements Comparator<student>{
+    
+    @Override
+    public int compare(student s1 , student s2){
+        if(s1.maths < s2.maths){
+            return -1;
+        }
+        
+        else if(s1.maths > s2.maths){
+            return 1;
+        }
+        else{
+            return s1.physics-s2.physics;
+        }
+    }
+    
+    //OR
+    // @Override
+    // public int compare(student s1 , student s2){
+    //     if(s1.maths == s2.maths){
+    //         return s1.physics-s2.physics;
+    //     }
+    //     return s1.maths-s2.maths;
+    // }
+    
+}
+class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+        
+        List<student> ls = new ArrayList<>();
+        ls.add(new student(70,80));
+        ls.add(new student(80,90));
+        ls.add(new student(50,60));
+        ls.add(new student(60,40));
+        ls.add(new student(50,80));
+        PriorityQueue<student> pq= new PriorityQueue<>(new cmp());
+        for(student s : ls){
+            pq.add(s);
+        }
+        int k = 3;
+        while(true){
+            if(k==0){
+                break;
+            }
+            student s1 = pq.remove();
+            System.out.println(s1.maths +"-" + s1.physics);
+            k--;
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
