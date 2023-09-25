3f5a0a97d8578f0b97c62e0c9e0b113985a9abee
Sangramsinh Sawant <49286586+sangramsinh123@users.noreply.github.com>
2023-09-25 15:45:32 +0530

    Add files via upload

---
+++ java Codes/Sieve/solve.java
@@ -0,0 +1,17 @@
+class Sieve{
+    public static void soe(int n){
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for(int i = 2;i<=n;i++){
+            if(isPrime[i] == true){
+                for(int j = i*i;j<=n;j+=i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        
+    }
+}
