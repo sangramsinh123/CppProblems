48b51cad9535e8d825f17067e6e4f0b52c2728dc
Sangramsinh Sawant <49286586+sangramsinh123@users.noreply.github.com>
2024-01-18 16:51:51 +0530

    Template

---
+++ CP-Template/Template.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define pii pair<int,int>
+#define vi vector<int>
+#define pb push_back
+#define F first
+#define S second
+#define inf INT_MAX
+#define N_inf INT_MIN
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+const int mod = (int)1e9+7;
+
+
+
+void solve(){
+	
+}
+
+
+int main(){
+
+	fastIO
+
+	int t ;
+	// cin>>t;
+	t = 1;
+	while(t > 0){
+		solve();
+		t--;
+	}
+
+	return 0;
+}
