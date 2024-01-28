66c9007a38412b5f204fd7b0680b3e9da10f7479
Sangramsinh Sawant <49286586+sangramsinh123@users.noreply.github.com>
2024-01-28 17:35:01 +0530

    Update Template.cpp

--- CP-Template/Template.cpp
+++ CP-Template/Template.cpp
@@ -15,40 +15,41 @@ const int mod = (int)1e9 + 7;
 ll min(ll a, ll b) { if (a >= b) { return b; } return a; }
 ll max(ll a, ll b) { if (a >= b) { return a; } return b; }
 
-ll ModAdd(ll a, ll b , ll mod) {
-	return (a % mod + b % mod) % mod;
-}
-
-ll ModSubtract(ll a, ll b , ll mod) {
-	return ((a % mod) - (b % mod) + mod) % mod;
-}
-
-ll ModMultiply(ll a, ll b , ll mod) {
-	return (a % mod * b % mod) % mod;
-}
-
-ll ModDivide(ll a, ll b , ll mod) {
-	return (a % mod * ModMultiply(b, mod - 2, mod)) % mod;
-}
+ll ModAdd(ll a, ll b , ll mod) { return (a % mod + b % mod) % mod; }
+ll ModSubtract(ll a, ll b , ll mod) { return ((a % mod) - (b % mod) + mod) % mod; }
+ll ModMultiply(ll a, ll b , ll mod) { return (a % mod * b % mod) % mod; }
+// ll ModDivide(ll a, ll b , ll mod) { return (a % mod * BinExpo(b, mod - 2, mod)) % mod; }
 
 
 
-void solve(){
-	
-}
-
-
-int main(){
-
-	fastIO
-
-	int t ;
-	// cin>>t;
-	t = 1;
-	while(t > 0){
-		solve();
-		t--;
+ll BinExpo(ll base , ll pow, ll mod) {
+	// base Cond
+	if (pow == 0) {
+		return 1;
+	}
+	if (pow == 1) {
+		return base;
+	}
+	ll newBase = BinExpo(base, pow / 2, mod);
+	ll ans = 1;
+	if (pow % 2) {
+		ans = ((newBase % mod) * (newBase % mod)) % mod;
+		ans = ((ans % mod) * (base % mod)) % mod;
+	}
+	else {
+		ans = ((newBase % mod) * (newBase % mod)) % mod;
 	}
 
+	return ans;
+
+}
+
+ll ModDivide(ll a, ll b , ll mod) { return (a % mod * BinExpo(b, mod - 2, mod)) % mod; }
+
+
+int main() {
+	cout << BinExpo(3, 1e5, 53);
+	// cout << s.size() << " ";
 	return 0;
 }
+
