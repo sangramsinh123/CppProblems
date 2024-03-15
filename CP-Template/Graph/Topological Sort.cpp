65f3f4fa81e883569a41ba12d7c82ae0155672d6
Sangramsinh Sawant <49286586+sangramsinh123@users.noreply.github.com>
2024-03-15 11:22:27 +0530

    Update Topological Sort.cpp

--- CP-Template/Graph/Topological Sort.cpp
+++ CP-Template/Graph/Topological Sort.cpp
@@ -1,1 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+void bfs(int n) {
+
+	vector<lli> indegree(n);
+	queue<int> q;
+	for (int i = 0; i < n; i++) {
+		if (indegree[i] == 0) {
+			q.push(i);
+		}
+	}
+	int visited = 0;
+	while (!q.empty()) {
+		int front = q.front();
+		q.pop();
+		visited++;
+		for (int child : adj[front]) {
+			indegree[child]--;
+			if (indegree[child] == 0) {
+				q.push(child);
+			}
+		}
+	}
+
+
+	if (visited == n) {
+		cout << "NO CYCLE" << endl;
+	}
+	else {
+		cout << "CYCLE" << endl;
+	}
+
+
+}
+
+
+int main() {
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		solve();
+		cout << endl;
+	}
+	return 0;
+}
 
